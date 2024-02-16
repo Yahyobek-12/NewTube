534727bb6f52dff88a54f428067f9cd855c96df8
Yahyobek <caisophiya@gmail.com>
2024-02-16 19:20:58 +0500

    NWT

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,33 +5,35 @@ import { Link } from 'react-router-dom'
 import { GlobalContext } from '../provider/provider'
 
 const Home = () => {
-  const { homeVideos, loading } = useContext(GlobalContext)
+  const { homeVideos, loading } = useContext(GlobalContext);
 
   return (
     <div className='home'>
-        <h2 className='home-logo'>New Videos</h2>
-        <div className="home-in">
-            {loading ? (
-              <div className='loading-home-card'>
-                <div className='loading'></div>
-              </div>
-            ): (
-              <>
-                {homeVideos.map(item => (
-                  <Link to='' className="home-card animate__animated animate__fadeIn" key={item.id}>
-                    <img src={item.snippet.thumbnails.standard.url} alt='video' />
-                    <div className="home-card-txt">
-                      <h4>{item.snippet.title.slice(0, 80)}</h4>
-                      <h5>{item.snippet.channelTitle}</h5>
-                      <div>{item.snippet.channelTitle.slice(0, 1)}</div>
-                    </div>
-                  </Link>
-                ))}
-              </>
-            )}
-        </div>
+      <h2 className='home-logo'>New Videos</h2>
+      <div className="home-in">
+        {loading ? (
+          <div className='loading-home-card'>
+            <div className='loading'></div>
+          </div>
+        ) : (
+          <>
+            {homeVideos.map(item => (
+              <Link to='' className="home-card animate__animated animate__fadeIn" key={item.id}>
+                {item.snippet && item.snippet.thumbnails && item.snippet.thumbnails.standard && (
+                  <img src={item.snippet.thumbnails.standard.url} alt='video' />
+                )}
+                <div className="home-card-txt">
+                  <h4>{item.snippet && item.snippet.title && item.snippet.title.slice(0, 80)}</h4>
+                  <h5>{item.snippet && item.snippet.channelTitle}</h5>
+                  <div>{item.snippet && item.snippet.channelTitle && item.snippet.channelTitle.slice(0, 1)}</div>
+                </div>
+              </Link>
+            ))}
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
